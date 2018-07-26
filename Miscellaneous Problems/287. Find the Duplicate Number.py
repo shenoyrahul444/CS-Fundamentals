dd5333b58f6b780e5f664347f32f6cce03869367
Rahul <shenoyrahul444@gmail.com>
2018-07-25 21:55:25 -0700

    Finding Duplicate in array

---
+++ Miscellaneous Problems/287. Find the Duplicate Number.py
@@ -0,0 +1,36 @@
+"""
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+
+
+"""
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i,num in enumerate(nums):
+            if nums[abs(num)] > 0:
+                nums[abs(num)] *= (-1)
+            else:
+                return abs(num)
+
+print(Solution().findDuplicate([3,1,3,4,2]))
+print(Solution().findDuplicate([1,3,4,2,2]))
